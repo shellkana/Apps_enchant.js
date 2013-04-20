7e9a0453e216e1d1f8337f8d95582d332a97055e
Shuya Kanamaru <shellkana@gmail.com>
2013-04-20 21:42:45 +0900

    add draw func

--- aruco/main.js
+++ aruco/main.js
@@ -92,15 +92,69 @@ window.onload = function() {
         root.backgroundColor = "red";
         cvl.addChild(root);
         var surface = new Surface(480, 320);
+        context = surface.context;
         root.image = surface;
         //game.rootScene.addChild(root);
         function snapshot() {
-            surface.context.drawImage(video, 0, 0, 480, 320);
-            imageData = surface.context.getImageData(0, 0, 480, 320);
+            context.drawImage(video, 0, 0, 480, 320);
+            imageData = context.getImageData(0, 0, 480, 320);
+            var markers = detector.detect(imageData);
+            drawCorners(markers);
+            drawId(markers);
         }
+
+        function drawCorners(markers) {
+            var corners, corner, i, j;
+
+            context.lineWidth = 3;
+
+            for ( i = 0; i !== markers.length; ++i) {
+                corners = markers[i].corners;
+
+                context.strokeStyle = "red";
+                context.beginPath();
+
+                for ( j = 0; j !== corners.length; ++j) {
+                    corner = corners[j];
+                    context.moveTo(corner.x, corner.y);
+                    corner = corners[(j + 1) % corners.length];
+                    context.lineTo(corner.x, corner.y);
+                }
+
+                context.stroke();
+                context.closePath();
+
+                context.strokeStyle = "green";
+                context.strokeRect(corners[0].x - 2, corners[0].y - 2, 4, 4);
+            }
+        }
+
+        function drawId(markers) {
+            var corners, corner, x, y, i, j;
+
+            context.strokeStyle = "blue";
+            context.lineWidth = 1;
+
+            for ( i = 0; i !== markers.length; ++i) {
+                corners = markers[i].corners;
+
+                x = Infinity;
+                y = Infinity;
+
+                for ( j = 0; j !== corners.length; ++j) {
+                    corner = corners[j];
+
+                    x = Math.min(x, corner.x);
+                    y = Math.min(y, corner.y);
+                }
+
+                context.strokeText(markers[i].id, x, y)
+            }
+        }
+
         var ita = new PlaneXY(8.5);
         ita.z = -40;
-        ita.scaleX=1.5;
+        ita.scaleX = 1.5;
         ita.mesh.texture.src = cvl._element;
         scene.addChild(ita);
         optimizeSprite3dForTextureScene(ita);
